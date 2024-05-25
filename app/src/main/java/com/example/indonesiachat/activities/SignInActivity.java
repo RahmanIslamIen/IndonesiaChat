a66c755048431a54e4f60ad3af5d7e1065314751
Isati Gulo <411211057@mahasiswa.undira.ac.id>
2024-05-25 13:04:35 +0700

    first commit

---
+++ app/src/main/java/com/example/indonesiachat/activities/SignInActivity.java
@@ -0,0 +1,31 @@
+package com.example.indonesiachat.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.indonesiachat.R;
+import com.example.indonesiachat.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/example/indonesiachat/activities/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.example.indonesiachat.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.indonesiachat.R;
+import com.example.indonesiachat.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
