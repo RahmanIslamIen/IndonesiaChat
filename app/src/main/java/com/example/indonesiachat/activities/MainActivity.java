f7edad0f4b489d05df718ff3448abcaab249d1ed
Rahman Islam <rahmanislamien@gmail.com>
2024-05-31 21:38:37 +0700

    fitur deteksi online user

--- app/src/main/java/com/example/indonesiachat/activities/MainActivity.java
+++ app/src/main/java/com/example/indonesiachat/activities/MainActivity.java
@@ -45,7 +45,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ConversionListener {
+public class MainActivity extends BaseActivity implements ConversionListener {
 
     private ActivityMainBinding binding;
     private PreferenceManager preferenceManager;
